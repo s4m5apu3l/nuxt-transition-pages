56e5a0d01da0b02bc79c06e13b76f2582476cd26
s4m5apu3l <asperatuz@yandex.ru>
2024-06-25 00:23:45 +0700

    feat: add some plugins

---
+++ pages/index.vue
@@ -0,0 +1,10 @@
+<script setup></script>
+
+<template>
+    <div class=" ">
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quis rerum eius odio velit porro fugit dolorem illo
+        nihil adipisci nulla in, dolor laborum mollitia tempore? Eum accusantium corrupti aperiam quasi?
+    </div>
+</template> 
+
+<style scoped></style>
