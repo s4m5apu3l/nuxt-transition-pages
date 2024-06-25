39e1becab73d87396d45cb575ef8d1bc01267989
s4m5apu3l <asperatuz@yandex.ru>
2024-06-26 00:52:10 +0700

    feat: add detail project page

--- layouts/custom.vue
+++ layouts/custom.vue
@@ -1,18 +1,18 @@
 <script setup>
 const props = defineProps({
-    title: String,
-    pageName: String,
+	title: String,
+	pageName: String,
 });
 </script>
 
 <template>
-    <div :class="`page ${props.pageName}`">
-        <base-header />
-        <div class="l-wrapper">
-            <page-title :title="props.title" />
-            <slot />
-        </div>
-    </div>
+	<div :class="`page ${props.pageName}`" style="padding-bottom: 24px">
+		<base-header />
+		<div class="l-wrapper">
+			<page-title :title="props.title" />
+			<slot />
+		</div>
+	</div>
 </template>
 
 <style scoped></style>
--- pages/gallery/[id].vue
+++ pages/gallery/[id].vue
@@ -7,7 +7,29 @@ const state = useGeneralState();
 	<div :class="state.pageBg">
 		<nuxt-layout name="custom" :title="`project ${route.params.id}`" :page-name="`project-${route.params.id}`">
 			<div class="page-content">
-				<div v-for="i in 4" :key="i" class="page-content__block"></div>
+				<div v-for="i in 2" :key="i" class="page-content__block">
+					<div class="page-content__block-photo">
+						<nuxt-img
+							:src="'https://814a7bc2-0fbd-4f94-9849-82da7e26f1b4.selstorage.ru/placeholders/placeholder_rectangle.webp'"
+							alt="img"
+							loading="lazy"
+							format="webp"
+						/>
+					</div>
+					<div class="page-content__block-text">
+						<p class="p1">
+							Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae nisi, velit asperiores eaque
+							ipsum, quis cupiditate libero aliquam repellendus aliquid ea. Deserunt voluptatem ad nemo
+							aliquam iste dolorum dolores cumque!
+						</p>
+					</div>
+				</div>
+
+				<div class="page-next">
+					<nuxt-link class="hover-fadein h2" :to="`/gallery/${Number(route.params.id) + 1}`">
+						next project - {{ Number(route.params.id) + 1 }}
+					</nuxt-link>
+				</div>
 			</div>
 		</nuxt-layout>
 	</div>
@@ -15,6 +37,9 @@ const state = useGeneralState();
 
 <style lang="scss" scoped>
 .page-content__block {
-	padding-bottom: 16px;
+	padding-bottom: 48px;
+	&-text {
+		margin-top: 8px;
+	}
 }
 </style>
