38330e1a0a99d64d6c70462aa7e355ed760406f9
s4m5apu3l <asperatuz@yandex.ru>
2024-06-26 23:09:34 +0700

    feat: add smoothScroll

---
+++ composables/useSmoothScroll.js
@@ -0,0 +1,19 @@
+import Lenis from '@studio-freight/lenis';
+
+export const useSmoothScroll = () => {
+	// idemo u store/index.js da dodamo scrollLenis
+	general.scrollLenis = new Lenis({
+		duration: 1.2,
+		orientation: 'vertical',
+		gestureOrientation: 'vertical',
+		smoothWheel: true,
+	});
+
+	general.scrollLenis.on('scroll', ScrollTrigger.update);
+
+	gsap.ticker.add(time => general.scrollLenis.raf(time * 1000));
+
+	gsap.ticker.lagSmoothing(0);
+	general.scrollLenis.scrollTo(0);
+	// general.scrollLenis.stop()
+};
