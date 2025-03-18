93e742f2a25ff915308531e170887969d959ccb6
slava <asperatuz@yandex.ru>
2025-03-18 20:58:16 +0700

    Updates

---
+++ assets/BBPou9yC.js
@@ -0,0 +1,1 @@
+import{u as _,c,b as e,w as m,g as t,n as u,h as r,o as p,M as l}from"./GUFsSGph.js";import{u as i}from"./C_aIbIOQ.js";const B={__name:"index",setup(x){const a=_(),n=i();return(f,g)=>{const o=l,s=r;return p(),c("div",{class:u(t(n).pageBg)},[e(s,{name:"custom",title:"home",pageName:t(a).name},{default:m(()=>[e(o)]),_:1},8,["pageName"])],2)}}};export{B as default};
