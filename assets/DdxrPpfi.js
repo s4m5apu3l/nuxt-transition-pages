df1e4738f7a254e6be7b15f5366133a205064c85
s4m5apu3l <aslavadoma@tuta.io>
2025-03-21 13:36:01 +0700

    Updates

--- assets/DdxrPpfi.js
+++
@@ -1,1 +0,0 @@
-import{u as _,c,b as e,w as u,g as t,n as m,h as r,o as p,M as l}from"./B0DMvbhw.js";import{u as i}from"./CUgz2bdf.js";const B={__name:"index",setup(x){const a=_(),n=i();return(d,f)=>{const o=l,s=r;return p(),c("div",{class:m(t(n).pageBg)},[e(s,{name:"custom",title:"iind",pageName:t(a).name},{default:u(()=>[e(o)]),_:1},8,["pageName"])],2)}}};export{B as default};
